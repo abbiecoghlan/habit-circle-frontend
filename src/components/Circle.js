a5d9b9a75d81345cf22f97a2704bdb1759f22039
“Abbie <abbie.coghlan@gmail.com>
2021-03-20 17:02:33 -0500

    added useContext and useReducer hooks

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,22 +1,32 @@
 import * as React from 'react';
 import * as d3 from "d3";
 import DaySlice from './DaySlice'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, useContext } from 'react'
+import { ProgressContext } from '../context/progress'
+import { UserContext } from '../context/user'
+import { DateContext } from '../context/date';
+
 
 
  const Circle = () => {
+    const [progressArray, setProgressArray] = useContext(ProgressContext)
+    const [currentUser, setCurrentUser] = useContext(UserContext)
 
 
+    const { currentMonth, currentYear, daysOfMonth, daysArray, setDaysArray, incrementMonth, decrementMonth} = useContext(DateContext)
+
+    // const testRef = useRef(0)
 
     
 
-    const [progressArray, setProgressArray] = useState([])
+    // const [progressArray, setProgressArray] = useState([])
     // const [currentMonth, setCurrentMonth] = useState(new Date().getMonth() + 1)
-    const [currentMonth, setCurrentMonth] = useState(new Date().getMonth() + 1)    
-    const [currentYear, setCurrentYear] = useState(new Date().getFullYear())
-    const [daysOfMonth, setDaysofMonth] = useState(new Date(currentYear, currentMonth, 0).getDate())
-    const [daysArray, setDaysArray] = useState([0])
-    const [currentUser, setUser] = useState([])
+
+    // const [currentMonth, setCurrentMonth] = useState(new Date().getMonth() + 1)    
+    // const [currentYear, setCurrentYear] = useState(new Date().getFullYear())
+    // const [daysOfMonth, setDaysofMonth] = useState(new Date(currentYear, currentMonth, 0).getDate())
+    // const [daysArray, setDaysArray] = useState([0])
+
     const [habits, setHabits] = useState([])
 
 
@@ -36,8 +46,8 @@ import { useEffect, useState } from 'react'
         .then(r => r.json())
         .then(data => {
             setProgressArray(data)
-            console.table(data)
-            console.log(currentMonth, currentYear, daysOfMonth)
+            // console.table(data)
+            // console.log(currentMonth, currentYear, daysOfMonth)
             const array = [10]
             while (array.length <= daysOfMonth){
                 array.push(1)
@@ -64,14 +74,14 @@ import { useEffect, useState } from 'react'
             .then(r => r.json())
             .then(data => {
                 setHabits(data.user.habits)
-                setUser(data.user)
+                setCurrentUser(data.user)
                 // const user = data.user
                 // console.log(currentMonth, currentYear, daysOfMonth)
-                const array = [10]
-                while (array.length <= daysOfMonth){
-                    array.push(1)
-                    setDaysArray([...array])        
-                    }
+                // const array = [10]
+                // while (array.length <= daysOfMonth){
+                //     array.push(1)
+                //     setDaysArray([...array])        
+                //     }
                      }) 
     
     
@@ -80,8 +90,14 @@ import { useEffect, useState } from 'react'
             
 
 
-
-
+useEffect(() => {
+    const array = [10]
+    while (array.length <= daysOfMonth){
+        array.push(1)
+        setDaysArray([...array])        
+        }
+}, [currentMonth])
+                    
 
 
 
@@ -97,12 +113,17 @@ import { useEffect, useState } from 'react'
     let pie = d3.pie()(daysArray)
 
     return (
+        <>
+        <button onClick={() => decrementMonth()} > Back </button>
+        <button onClick={() => incrementMonth()}  >Forward</button>
+        <div>
         <svg height={height} width={width}>
             <g transform={`translate(${width / 2},${height / 2}) rotate(245 0 0)`}>
-        
                 <DaySlice habits={habits} progressArray={progressArray} pie={pie}/>
             </g>
         </svg> 
+        </div>
+        </>
     )
 
 }
