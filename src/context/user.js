074c65d23c01c704a3df158f2d5c8d645e7aa1b6
“Abbie <abbie.coghlan@gmail.com>
2021-05-11 20:28:51 -0500

    clean up user context

--- src/context/user.js
+++ src/context/user.js
@@ -139,23 +139,3 @@ function UserProvider({ children }) {
 
 // export
 export { UserContext, UserProvider }
-
-
-
-
-// PREVIOUS 
-// create the context object
-// const UserContext = createContext()
-
-// create the context provider component
-// function UserProvider({ children }) {
-
-//     const [currentUser, setCurrentUser] = useState([])
-//     const value = [currentUser, setCurrentUser]
-
-//     return (
-//     <UserContext.Provider value={value}>
-//         {children}
-//     </UserContext.Provider>
-//     )
-// }
