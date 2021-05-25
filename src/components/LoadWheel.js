d1c0eca50176623f69030831c4eb542d85f92bfb
“Abbie <abbie.coghlan@gmail.com>
2021-05-25 18:16:59 -0500

    load wheel

--- src/components/LoadWheel.js
+++ src/components/LoadWheel.js
@@ -1,20 +1,16 @@
 
-import { Dimmer, Segment, Loader} from 'semantic-ui-react'
+import { Dimmer, Loader} from 'semantic-ui-react'
 import '../App.css'
-import Circle from './Circle';
 
-
-
-
-  const LoadWheel = (props) => {
-    return (
-      <div class="ui center aligned middle aligned grid" style={{height: "100vh"}}>
-        <Dimmer active inverted inline="center">
-          <Loader indeterminate size='massive'> Loading your habit data </Loader>
-        </Dimmer> 
-      </div>      
-      )
-  }
+const LoadWheel = () => {
+  return (
+    <div class="ui center aligned middle aligned grid" style={{height: "100vh"}}>
+      <Dimmer active inverted inline="center">
+        <Loader indeterminate size='massive'> Loading your habit data </Loader>
+      </Dimmer> 
+    </div>      
+    )
+}
 
 
 export default LoadWheel
